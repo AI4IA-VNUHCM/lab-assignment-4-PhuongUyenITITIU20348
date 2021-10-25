7d67a4cefa1bdf8922e318b19da9057908269386
PhuongUyenITITIU20348 <ititiu20348@student.hcmiu.edu.vn>
2021-10-25 20:57:06 +0700

    ex1

--- src/Exercise1.c
+++ src/Exercise1.c
@@ -17,7 +17,20 @@ ______________________________________
 
 void Ex1(int n){
 	//Your codes here
-    
+    int arr[10][10];
+    for (int i = 0; i <= n; i++)
+	{
+	for (int j = 0; j <= i; j++)
+	{ if (i == j || j == 0) 
+	{ arr[i][j] = 1;
+	} 
+	else 
+	{ arr[i][j]= arr[i-1][j-1]+ arr[i-1][j];
+	}
+	printf("%d ", arr[i][j]);
+	}
+	printf("\n");
+}
 }
 
 int main(int argc, char *argv[]) {
